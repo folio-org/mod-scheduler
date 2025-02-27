0dce499d4dd9c2743464a839142a05e15329a7ac
Mykola Makhin <161492190+mykolam-epm@users.noreply.github.com>
2025-02-27 15:39:25 +0200

    MODSCHED-25 (#136)
    Co-authored-by: PBobylev <pavel_bobylev@epam.com>

--- src/main/java/org/folio/scheduler/integration/kafka/model/EntitlementEventType.java
+++ src/main/java/org/folio/scheduler/integration/kafka/model/EntitlementEventType.java
@@ -1,6 +1,10 @@
 package org.folio.scheduler.integration.kafka.model;
 
+import static com.github.jknack.handlebars.internal.lang3.StringUtils.isBlank;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import java.util.stream.Stream;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -14,5 +18,13 @@ public enum EntitlementEventType {
 
   @JsonValue
   private final String value;
+
+  @JsonCreator
+  public static EntitlementEventType of(String value) {
+    if (isBlank(value)) {
+      return null;
+    }
+    return Stream.of(values()).filter(val -> val.getValue().equalsIgnoreCase(value)).findAny().orElse(null);
+  }
 }
 
