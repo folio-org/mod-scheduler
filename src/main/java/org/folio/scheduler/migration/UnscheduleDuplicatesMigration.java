0982209f522d120528d48e0afd1d7bd41739decd
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2025-08-14 20:43:43 +0300

    feat(checkstyle): add method length checkstyle rule and refactor code for compliance (#170)

--- src/main/java/org/folio/scheduler/migration/UnscheduleDuplicatesMigration.java
+++ src/main/java/org/folio/scheduler/migration/UnscheduleDuplicatesMigration.java
@@ -2,7 +2,6 @@ package org.folio.scheduler.migration;
 
 import java.util.HashSet;
 import liquibase.database.Database;
-import liquibase.exception.CustomChangeException;
 import lombok.extern.log4j.Log4j2;
 import org.quartz.JobKey;
 import org.quartz.Scheduler;
@@ -12,24 +11,26 @@ import org.springframework.transaction.annotation.Transactional;
 @Log4j2
 public class UnscheduleDuplicatesMigration extends AbstractCustomTaskChangeMigration {
 
+  private static final String SELECT_DUPLICATE_TIMERS = """
+    SELECT
+      id
+    FROM (
+      SELECT
+        id,
+        row_number() over (partition by natural_key order by id) as rn
+      FROM
+        timer
+      WHERE timer_descriptor->'enabled' = 'true'
+    ) AS tmp
+    WHERE
+      rn > 1
+    """;
+
   @Override
   @Transactional
-  public void execute(Database database) throws CustomChangeException {
+  public void execute(Database database) {
     var idsOfTimersToUnschedule = new HashSet<String>();
-    runQuery(database, """
-      SELECT
-        id
-      FROM (
-        SELECT
-          id,
-          row_number() over (partition by natural_key order by id) as rn
-        FROM
-          timer
-        WHERE timer_descriptor->'enabled' = 'true'
-      ) AS tmp
-      WHERE
-        rn > 1
-      """, resultSet -> idsOfTimersToUnschedule.add(resultSet.getString("id")));
+    runQuery(database, SELECT_DUPLICATE_TIMERS, resultSet -> idsOfTimersToUnschedule.add(resultSet.getString("id")));
 
     if (!idsOfTimersToUnschedule.isEmpty()) {
       log.info("Found {} duplicate timers - unscheduling", idsOfTimersToUnschedule.size());
