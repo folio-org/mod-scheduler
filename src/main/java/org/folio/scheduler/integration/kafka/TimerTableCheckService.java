623a79296cff88d47e434d202c06a1f854466841
Dmytro Tkachenko <dmytro_tkachenko@epam.com>
2026-01-24 12:06:09 +0200

    MODSCHED-58  Sunflower SP#5 - Kafka event processing improvements (#201)

--- src/main/java/org/folio/scheduler/integration/kafka/TimerTableCheckService.java
+++ src/main/java/org/folio/scheduler/integration/kafka/TimerTableCheckService.java
@@ -1,28 +1,32 @@
 package org.folio.scheduler.integration.kafka;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import javax.sql.DataSource;
-import org.folio.spring.FolioExecutionContext;
-import org.springframework.dao.DataRetrievalFailureException;
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
 
+import java.sql.ResultSet;
+import lombok.extern.log4j.Log4j2;
+import org.folio.spring.FolioExecutionContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Log4j2
 public class TimerTableCheckService {
 
-  private static final String[] TABLE_TYPE = {"TABLE"};
   private static final String TIMER_TABLE_NAME = "timer";
+  private static final String TABLE_EXIST_SQL = """
+    SELECT EXISTS (
+      SELECT 1 FROM information_schema.tables
+        WHERE table_schema = ? AND table_name = ?)
+    """;
 
-  private final DataSource dataSource;
+  private final JdbcTemplate jdbcTemplate;
   private final FolioExecutionContext context;
   private final TableNameCase tableNameCase;
 
-  public TimerTableCheckService(DataSource dataSource, FolioExecutionContext context) {
-    this(dataSource, context, TableNameCase.LOWER);
+  public TimerTableCheckService(JdbcTemplate jdbcTemplate, FolioExecutionContext context) {
+    this(jdbcTemplate, context, TableNameCase.LOWER);
   }
 
-  public TimerTableCheckService(DataSource dataSource, FolioExecutionContext context, TableNameCase tableNameCase) {
-    this.dataSource = dataSource;
+  public TimerTableCheckService(JdbcTemplate jdbcTemplate, FolioExecutionContext context, TableNameCase tableNameCase) {
+    this.jdbcTemplate = jdbcTemplate;
     this.context = context;
     this.tableNameCase = tableNameCase;
   }
@@ -32,21 +36,26 @@ public class TimerTableCheckService {
   }
 
   private boolean tableExists(String tableName) {
-    try {
-      try (Connection connection = dataSource.getConnection()) {
-        DatabaseMetaData metaData = connection.getMetaData();
+    var schema = getDbSchemaName();
+    var table = tableNameCase.format(tableName);
 
-        var schema = getDbSchemaName();
-        try (ResultSet resultSet = metaData.getTables(null, schema, tableNameCase.format(tableName), TABLE_TYPE)) {
-          return resultSet.next();
-        }
-      }
-    } catch (SQLException e) {
-      throw new DataRetrievalFailureException("Failed to check if table " + tableName + " exists", e);
-    }
+    log.info("Checking if table exists in schema: table = {}, schema = {}. Thread: {}", table, schema,
+      Thread.currentThread().getName());
+
+    var found = isTrue(jdbcTemplate.query(
+      TABLE_EXIST_SQL,
+      (ResultSet resultSet) -> resultSet.next() && resultSet.getBoolean(1),
+      schema, table)
+    );
+
+    log.info("Table existence check result: table = {}, schema = {}, exists = {}. Thread: {}", table, schema,
+      found, Thread.currentThread().getName());
+    return found;
   }
 
   private String getDbSchemaName() {
+    log.debug("FolioExecutionContext in use when getting DB schema name: tenantId = {}. Thread: {}",
+      context.getTenantId(), Thread.currentThread().getName());
     return context.getFolioModuleMetadata().getDBSchemaName(context.getTenantId());
   }
 
